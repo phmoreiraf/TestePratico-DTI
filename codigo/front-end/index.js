3ed76a83519969a29587595639e968f54790dadf
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-03-25 23:03:57 -0300

    Update index.js

--- codigo/front-end/index.js
+++ codigo/front-end/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -12,4 +12,5 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+// comment out or remove the following line if reportWebVitals function is missing
+// reportWebVitals();
