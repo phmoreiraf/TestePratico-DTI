9da1c11efe50113e5b7a5d4c803e2b1ed3d0b995
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-03-26 20:54:11 -0300

    att

--- codigo/front-end/src/formulario.js
+++ codigo/front-end/src/formulario.js
@@ -17,72 +17,96 @@ function Formulario({
         encontrarMelhorCanil();
     };
 
-    return (
-        <div className="container">
-            <div className="form-image">
-                <img src="img/dog.jpg" alt="" />
-            </div>
-            <div className="form">
-                <form id="intention-form" onSubmit={handleSubmit}>
-                    <div className="form-header">
-                        <div className="title">
-                            <h1>Formulário de consulta - Canil</h1>
-                        </div>
-                    </div>
+    return ( <
+        div className = "container" >
+        <
+        div className = "form-image" >
+        <
+        img src = "img/dog.jpg"
+        alt = "" / >
+        <
+        /div> <
+        div className = "form" >
+        <
+        form id = "intention-form"
+        onSubmit = { handleSubmit } >
+        <
+        div className = "form-header" >
+        <
+        div className = "title" >
+        <
+        h1 > Formulário de consulta - Canil < /h1> <
+        /div> <
+        /div>
 
-                    <div className="input-group">
-                        <div className="input-box">
-                            <label htmlFor="data">Data</label>
-                            <input 
-                                id="data" 
-                                type="date" 
-                                name="data" 
-                                placeholder="Digite a data" 
-                                value={data}
-                                onChange={({ target: { value } }) => setData(value)}
-                                required
-                            />
-                        </div>
+        <
+        div className = "input-group" >
+        <
+        div className = "input-box" >
+        <
+        label htmlFor = "data" > Data < /label> <
+        input id = "data"
+        type = "date"
+        name = "data"
+        placeholder = "Digite a data"
+        value = { data }
+        onChange = {
+            ({ target: { value } }) => setData(value) }
+        required /
+        >
+        <
+        /div>
 
-                        <div className="input-box">
-                            <label htmlFor="caesPequenos">Quantidade de Cães Pequenos</label>
-                            <input 
-                                id="caesPequenos" 
-                                type="number" 
-                                name="caesPequenos" 
-                                placeholder="Digite a quantidade de Cães Pequenos" 
-                                value={caesPequenos}
-                                onChange={({ target: { value } }) => setCaesPequenos(value)}
-                                required
-                            />
-                        </div>
+        <
+        div className = "input-box" >
+        <
+        label htmlFor = "caesPequenos" > Quantidade de Cães Pequenos < /label> <
+        input id = "caesPequenos"
+        type = "number"
+        name = "caesPequenos"
+        placeholder = "Digite a quantidade de Cães Pequenos"
+        value = { caesPequenos }
+        onChange = {
+            ({ target: { value } }) => setCaesPequenos(value) }
+        required /
+        >
+        <
+        /div>
 
-                        <div className="input-box">
-                            <label htmlFor="caesGrandes">Quantidade de Cães Grandes</label>
-                            <input 
-                                id="caesGrandes" 
-                                type="number" 
-                                name="caesGrandes" 
-                                placeholder="Digite a quantidade de Cães Grandes" 
-                                value={caesGrandes}
-                                onChange={({ target: { value } }) => setCaesGrandes(value)}
-                                required
-                            />
-                        </div>
-                    </div>
+        <
+        div className = "input-box" >
+        <
+        label htmlFor = "caesGrandes" > Quantidade de Cães Grandes < /label> <
+        input id = "caesGrandes"
+        type = "number"
+        name = "caesGrandes"
+        placeholder = "Digite a quantidade de Cães Grandes"
+        value = { caesGrandes }
+        onChange = {
+            ({ target: { value } }) => setCaesGrandes(value) }
+        required /
+        >
+        <
+        /div> <
+        /div>
 
-                    <div className="continue-button">
-                        <button type="submit">Consultar</button>
-                    </div>
-                </form>
-            </div>
-            {melhorCanil && (
-                <div>
-                    O melhor canil é {melhorCanil.nome} com um custo total de{' '}
-                    {melhorCanil.custoTotal}.
-                </div>
-            )}
-        </div>
+        <
+        div className = "continue-button" >
+        <
+        button type = "submit" > Consultar < /button> <
+        /div> <
+        /form> {
+            melhorCanil && ( <
+                div >
+                O melhor canil é { melhorCanil.nome }
+                com um custo total de { ' ' } { melhorCanil.custoTotal }. <
+                /div>
+            )
+        } <
+        /div>
+
+        <
+        /div>
     );
 }
 
