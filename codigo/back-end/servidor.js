94b14fe2e644decd270d2554ce85cc495545cece
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-03-26 14:15:38 -0300

    att

--- codigo/back-end/servidor.js
+++ codigo/back-end/servidor.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 
 function encontrarMelhorCanil(data, caesPequenos, caesGrandes) {
     const canis = [
