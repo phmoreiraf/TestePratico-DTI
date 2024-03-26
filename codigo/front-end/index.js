659df3e66bcc80c7fcb0fde3626ebf5cc33bc9d0
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-03-25 22:09:28 -0300

    Update index.js

--- codigo/front-end/index.js
+++ codigo/front-end/index.js
@@ -1,35 +1,10 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Formulario from './formulario';
-import './petshop.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-function App() {
-    const [data, setData] = useState('');
-    const [caesPequenos, setCaesPequenos] = useState('');
-    const [caesGrandes, setCaesGrandes] = useState('');
-    const [melhorPetshop, setMelhorPetshop] = useState(null);
-
-    const encontrarMelhorPetshop = async() => {
-        const response = await axios.post('http://localhost:3000/melhor-petshop', {
-            data: data,
-            caesPequenos: caesPequenos,
-            caesGrandes: caesGrandes,
-        });
-
-        setMelhorPetshop(response.data);
-    };
-
-    return ( <
-        Formulario data = { data }
-        setData = { setData }
-        caesPequenos = { caesPequenos }
-        setCaesPequenos = { setCaesPequenos }
-        caesGrandes = { caesGrandes }
-        setCaesGrandes = { setCaesGrandes }
-        encontrarMelhorPetshop = { encontrarMelhorPetshop }
-        melhorPetshop = { melhorPetshop }
-        />
-    );
-}
-
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
