2a17e2ae3735f4815d3a9a6ea3cb19e05ea1ddcf
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-03-25 23:39:20 -0300

    Update formulario.js

--- codigo/front-end/src/formulario.js
+++ codigo/front-end/src/formulario.js
@@ -9,66 +9,60 @@ function Formulario({
     setCaesGrandes,
     encontrarMelhorPetshop,
     melhorPetshop,
+    loading,
 }) {
-    return ( <
-        div className = "container" >
-        <
-        h1 > PetShop < /h1> <
-        h2 > Formulário de consulta < /h2> <
-        form id = "intention-form"
-        onSubmit = { encontrarMelhorPetshop } >
-        <
-        label htmlFor = "data" > Data: < /label> <
-        input type = "date"
-        id = "data"
-        value = { data }
-        onChange = {
-            ({ target: { value } }) => setData(value)
-        }
-        required /
-        >
+    return (
+        <div className="container">
+            <h1>PetShop</h1>
+            <h2>Formulário de consulta</h2>
+            <form id="intention-form" onSubmit={encontrarMelhorPetshop}>
+                <label htmlFor="data">Data:</label>
+                <input
+                    type="date"
+                    id="data"
+                    value={data}
+                    onChange={({ target: { value } }) => setData(value)}
+                    required
+                />
 
-        <
-        label htmlFor = "caesPequenos" >
-        Quantidade de Cães Pequenos:
-        <
-        /label> <
-        input type = "number"
-        id = "caesPequenos"
-        value = { caesPequenos }
-        onChange = {
-            ({ target: { value } }) =>
-            setCaesPequenos(value)
-        }
-        required /
-        >
+                <label htmlFor="caesPequenos">
+                    Quantidade de Cães Pequenos:
+                </label>
+                <input
+                    type="number"
+                    id="caesPequenos"
+                    value={caesPequenos}
+                    onChange={({ target: { value } }) =>
+                        setCaesPequenos(value)
+                    }
+                    required
+                />
 
-        <
-        label htmlFor = "caesGrandes" >
-        Quantidade de Cães Grandes:
-        <
-        /label> <
-        input type = "number"
-        id = "caesGrandes"
-        value = { caesGrandes }
-        onChange = {
-            ({ target: { value } }) =>
-            setCaesGrandes(value)
-        }
-        required /
-        >
+                <label htmlFor="caesGrandes">
+                    Quantidade de Cães Grandes:
+                </label>
+                <input
+                    type="number"
+                    id="caesGrandes"
+                    value={caesGrandes}
+                    onChange={({ target: { value } }) =>
+                        setCaesGrandes(value)
+                    }
+                    required
+                />
 
-        <
-        button type = "submit" > Consultar < /button> < /
-        form > {
-            melhorPetshop && ( <
-                div >
-                O melhor petshop é { melhorPetshop.nome }
-                com um custo total de { ' ' } { melhorPetshop.custoTotal }. <
-                /div>
-            )
-        } <
-        /div>
+                <button type="submit" disabled={loading}>
+                    {loading ? 'Consultando...' : 'Consultar'}
+                </button>
+            </form>
+            {melhorPetshop && (
+                <div>
+                    <h2>Melhor Petshop:</h2>
+                    <p>Nome: {melhorPetshop.nome}</p>
+                    <p>Custo Total: {melhorPetshop.custoTotal}</p>
+                </div>
+            )}
+        </div>
     );
 }
 
