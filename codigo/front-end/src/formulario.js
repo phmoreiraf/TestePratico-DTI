951c1be24c19e4293e03d56f4d9ac4fb05e7910a
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-03-27 15:15:49 -0300

    ATT

--- codigo/front-end/src/formulario.js
+++ codigo/front-end/src/formulario.js
@@ -19,72 +19,95 @@ function Formulario({
         encontrarMelhorCanil();
     };
 
-    return (
-        <div className="container">
-            <div className="form-image">
-                <img src={MinhaImagem} alt="Imagem cachorro" />
-                <img src={MinhaImagem2} alt="Imagem cachorro2" />
-                <img src={MinhaImagem3} alt="Imagem cachorro3" />
-            </div>
-            <div className="form">
-                <form id="intention-form" onSubmit={handleSubmit}>
-                    <div className="form-header">
-                        <div className="title">
-                            <h1>Formulário de consulta - Melhor Canil</h1>
-                        </div>
-                    </div>
-                    <div className="input-group">
-                        <div className="input-box">
-                            <label htmlFor="data">Data</label>
-                            <input 
-                                id="data" 
-                                type="date" 
-                                name="data" 
-                                placeholder="Digite a data" 
-                                value={data}
-                                onChange={({ target: { value } }) => setData(value)}
-                                required
-                            />
-                        </div>
-                        <div className="input-box">
-                            <label htmlFor="caesPequenos">Quantidade de Cães Pequenos</label>
-                            <input 
-                                id="caesPequenos" 
-                                type="number" 
-                                name="caesPequenos" 
-                                placeholder="Digite a quantidade de Cães Pequenos" 
-                                value={caesPequenos}
-                                onChange={({ target: { value } }) => setCaesPequenos(value)}
-                                required
-                            />
-                        </div>
-                        <div className="input-box">
-                            <label htmlFor="caesGrandes">Quantidade de Cães Grandes</label>
-                            <input 
-                                id="caesGrandes" 
-                                type="number" 
-                                name="caesGrandes" 
-                                placeholder="Digite a quantidade de Cães Grandes" 
-                                value={caesGrandes}
-                                onChange={({ target: { value } }) => setCaesGrandes(value)}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className="continue-button">
-                        <button type="submit">Consultar</button>
-                    </div>
-                </form>
-                <br/>
-                <br/>
-                {melhorCanil && (
-                    <div>
-                        O melhor canil é <strong>{melhorCanil.nome}</strong> com um custo total de {' '}
-                        <strong> R${melhorCanil.custoTotal}</strong>.
-                    </div>
-                )}
-            </div>
-        </div>
+    return ( <
+        div className = "container" >
+        <
+        div className = "form-image" >
+        <
+        img src = { MinhaImagem }
+        alt = "Imagem cachorro" / >
+        <
+        img src = { MinhaImagem2 }
+        alt = "Imagem cachorro2" / >
+        <
+        img src = { MinhaImagem3 }
+        alt = "Imagem cachorro3" / >
+        <
+        /div> <
+        div className = "form" >
+        <
+        form id = "intention-form"
+        onSubmit = { handleSubmit } >
+        <
+        div className = "form-header" >
+        <
+        div className = "title" >
+        <
+        h1 > Formulário de consulta - Melhor PetShop < /h1> <
+        /div> <
+        /div> <
+        div className = "input-group" >
+        <
+        div className = "input-box" >
+        <
+        label htmlFor = "data" > Data < /label> <
+        input id = "data"
+        type = "date"
+        name = "data"
+        placeholder = "Digite a data"
+        value = { data }
+        onChange = {
+            ({ target: { value } }) => setData(value) }
+        required /
+        >
+        <
+        /div> <
+        div className = "input-box" >
+        <
+        label htmlFor = "caesPequenos" > Quantidade de Cães Pequenos < /label> <
+        input id = "caesPequenos"
+        type = "number"
+        name = "caesPequenos"
+        placeholder = "Digite a quantidade de Cães Pequenos"
+        value = { caesPequenos }
+        onChange = {
+            ({ target: { value } }) => setCaesPequenos(value) }
+        required /
+        >
+        <
+        /div> <
+        div className = "input-box" >
+        <
+        label htmlFor = "caesGrandes" > Quantidade de Cães Grandes < /label> <
+        input id = "caesGrandes"
+        type = "number"
+        name = "caesGrandes"
+        placeholder = "Digite a quantidade de Cães Grandes"
+        value = { caesGrandes }
+        onChange = {
+            ({ target: { value } }) => setCaesGrandes(value) }
+        required /
+        >
+        <
+        /div> <
+        /div> <
+        div className = "continue-button" >
+        <
+        button type = "submit" > Consultar < /button> <
+        /div> <
+        /form> <
+        br / >
+        <
+        br / > {
+            melhorCanil && ( <
+                div >
+                O melhor canil é < strong > { melhorCanil.nome } < /strong> com um custo total de {' '} <
+                strong > R$ { melhorCanil.custoTotal } < /strong>. <
+                /div>
+            )
+        } <
+        /div> <
+        /div>
     );
 }
 
