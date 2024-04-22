cecbd0b5f4565cc604f856cc33236ddf07bc54e5
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-04-22 20:55:40 -0300

    att

--- codigo/front-end/src/index.js
+++ codigo/front-end/src/index.js
@@ -4,8 +4,10 @@ import App from "./App.js";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>
 );
